80dab155996d7104ade8d16335c43d21ff58fd4c
ajweb <info@ajweb.es>
2011-04-16 21:36:01 +0200

    cleaning of removed files

--- config.fulltextSearch.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * autoloader components to instantiate slidingBanner contribution
- */
-$autoLoadConfig[220][] = array('autoType'=>'class', 'loadFile'=> 'observers/class.fulltextSearch.php');
-$autoLoadConfig[230][] = array('autoType'=>'classInstantiate', 'className'=> 'fullTextSearch', 'objectName'=> 'fullTextSearch');
-
-?>
